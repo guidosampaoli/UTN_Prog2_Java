cd4da9aa13679414d50b332f86673683fed02be2
Guido Sampaoli <guidosampaoli@gmail.com>
2025-11-28 11:25:38 -0300

    Update Javadoc for main method in tp07/e03

--- src/tp07_herencia_polimorfismo/e03_empleados_polimorfismo/Main.java
+++ src/tp07_herencia_polimorfismo/e03_empleados_polimorfismo/Main.java
@@ -3,17 +3,18 @@ package tp07_herencia_polimorfismo.e03_empleados_polimorfismo;
 import java.util.ArrayList;
 
 /**
- * <h2>Título</h2>
+ * <h2>3. Empleados y polimorfismo</h2>
  * <p>
- * Escribe un programa en Java que...<br>
- *
+ * ● Clase abstracta: Empleado con método calcularSueldo()<br>
+ * ● Subclases: EmpleadoPlanta, EmpleadoTemporal<br>
+ * ● Tarea: Crear lista de empleados, invocar calcularSueldo() polimórficamente, usar instanceof para clasificar
  * </p>
- *
  * @author Guido Sampaoli
  * @see
  * <a href="https://github.com/guidosampaoli/UTN_Prog2_Java/tree/main/src" target="_blank">GitHub
  * - guidosampaoli</a>
  */
+
 public class Main {
 
     public static void main(String[] args) {
